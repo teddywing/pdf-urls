612842e55541d112ae4dbf56d49a6026319d1165
Teddy Wing <git@teddywing.com>
2019-10-30 22:16:22 +0100

    Inspect an example PDF file using the 'lopdf' crate
    Walk the different objects in the PDF to discover how hyperlinks are
    stored and how I can access them.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate lopdf;
+
+use lopdf::{Document, Object};
+
 fn main() {
-    println!("Hello, world!");
+    let doc = Document::load("example.pdf").unwrap();
+    // dbg!("{:?}", doc);
+
+    for (id, obj) in doc.objects {
+        // dbg!(obj.as_dict().unwrap());
+
+        match obj {
+            // Object::String(bs, format) => {
+            //     dbg!(bs, format);
+            //     ()
+            // },
+            Object::Dictionary(d) => {
+                // dbg!(d);
+
+                for (k, v) in d.iter() {
+                    dbg!(::std::str::from_utf8(&k).unwrap(), v);
+                }
+
+                ()
+            },
+            _ => (),
+        }
+    }
 }
