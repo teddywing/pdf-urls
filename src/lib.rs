adff229c543b765b1bfd7cb6c871d6b89617eff0
Teddy Wing <git@teddywing.com>
2019-11-01 22:23:47 +0100

    lib: Use `std::str`
    Get rid of `::str`-prefixed calls.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate lopdf;
 
 use std::path::Path;
+use std::str;
 
 use lopdf::{Document, Object};
 
@@ -11,17 +12,17 @@ pub fn get_urls_from_pdf<P: AsRef<Path>>(path: P) {
         match obj {
             Object::Dictionary(d) => {
                 for (k, v) in d.iter() {
-                    let key = ::std::str::from_utf8(&k).unwrap();
+                    let key = str::from_utf8(&k).unwrap();
 
                     if key == "A" {
                         for (k, v) in v.as_dict().unwrap() {
-                            let key = ::std::str::from_utf8(&k).unwrap();
+                            let key = str::from_utf8(&k).unwrap();
 
                             if key == "URI" {
 
                                 match v {
                                     Object::String(s, _) => {
-                                        println!("{}", ::std::str::from_utf8(s).unwrap());
+                                        println!("{}", str::from_utf8(s).unwrap());
 
                                         ()
                                     },
