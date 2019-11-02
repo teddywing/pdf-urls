523170630fd8e9ba695f4167d132a8812d814a1e
Teddy Wing <git@teddywing.com>
2019-11-02 04:28:56 +0100

    main: Accept a file path as a command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+extern crate exitcode;
+
+use std::env;
+use std::process;
+
 use pdf_urls::get_urls_from_pdf;
 
+fn print_usage() {
+    println!("usage: pdf-urls FILE");
+}
+
 fn main() {
-    match get_urls_from_pdf("example.pdf") {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        print_usage();
+
+        process::exit(exitcode::USAGE);
+    }
+
+    match get_urls_from_pdf(&args[1]) {
         Ok(urls) => {
             for url in urls {
                 println!("{}", url);
