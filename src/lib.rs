99189e55a2fe4b14e269015f73b8d30a8e45ef2c
Teddy Wing <git@teddywing.com>
2019-11-02 05:39:21 +0100

    get_urls_from_pdf: Add a short doc string

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use lopdf::{Document, Object};
 
 use errors::Result;
 
+/// Given a file path to a PDF, return a Vec of all URLs in the document.
 pub fn get_urls_from_pdf<P: AsRef<Path>>(path: P) -> Result<Vec<String>> {
     let doc = Document::load(path)?;
 
